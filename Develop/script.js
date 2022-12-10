c089ddb6275bc15d67637212a982b26798e905bf
Bronson Wardle <bronson.wardle@gmail.com>
2022-12-10 11:46:46 -0500

    Verified all variables that will be used for the pw generator

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,35 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+//  Assignment Code
+ var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+
+//  Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+// Various Arrays 
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+// Variable Declaration 
+var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmNumericCharacter;
+var confirmUpperCase;
+var confirmLowerCase;
+
+
+
+//  Write password to the #password input
+ function writePassword() {
+   var password = generatePassword();
+ var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-}
+ }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
